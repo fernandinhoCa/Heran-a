6b4773ce9dfeb0e49421f6dbcfd9f6b0a9aa577d
Fernando Capeletti <fernando.capeletti@alunos.sc.senac.br>
2022-09-05 21:20:06 -0300

    Herança

---
+++ src/heranca/Heranca.java
@@ -0,0 +1,23 @@
+package heranca;
+
+/**
+ *
+ * @author fernando.capeletti
+ */
+public class Heranca {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Automovel carro = new Automovel();
+       Automovel moto = new Automovel(); 
+       
+       carro.setNumPlaca("GRO9B78");
+       carro.setNumPortas(3);
+       carro.setNumRodas(4);
+       carro.setCapacidade(5);
+       
+    }
+    
+}
---
+++ src/heranca/Transporte.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package heranca;
+
+/**
+ *
+ * @author fernando.capeletti
+ */
+public class Transporte {
+    private int capacidade;
+
+    public int getCapacidade() {
+        return capacidade;
+    }
+
+    public void setCapacidade(int capacidade) {
+        this.capacidade = capacidade;
+    }
+}
